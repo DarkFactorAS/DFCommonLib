09f22f33c357ec65a0c8d2f68c1c40d74b44bfbf
Thor Hanssen <thor.richard.hansen@gmail.com>
2025-08-11 19:26:52 +0200

    Code Review: Removed unused classes + made secrets input parameters to JWT class

--- DFCommonLib/HttpApi/DFRestServer.cs
+++ DFCommonLib/HttpApi/DFRestServer.cs
@@ -28,54 +28,5 @@ namespace DFCommonLib.HttpApi
             _logger.LogInformation("Ping received at {Time}", DateTime.UtcNow);
             return "PONG";
         }
-
-        [HttpPost("Login")]
-        [AllowAnonymous]
-        public IActionResult Login([FromBody] LoginRequest loginRequest)
-        {
-            if (loginRequest == null || string.IsNullOrEmpty(loginRequest.Username) || string.IsNullOrEmpty(loginRequest.Password))
-            {
-                return BadRequest("Invalid login request.");
-            }
-
-            // Simulate token generation (replace with actual implementation)
-            var token = GenerateJwtToken(loginRequest.Username);
-
-            return Ok(new { Token = token });
-        }
-
-        [Authorize]
-        [HttpGet("SecureEndpoint")]
-        public IActionResult SecureEndpoint()
-        {
-            return Ok("This is a secure endpoint.");
-        }
-
-        private string GenerateJwtToken(string username)
-        {
-            var securityKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("YourSecretKeyHere"));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new System.Security.Claims.ClaimsIdentity(new[]
-                {
-                    new System.Security.Claims.Claim("username", username)
-                }),
-                Expires = DateTime.UtcNow.AddHours(1),
-                SigningCredentials = credentials
-            };
-
-            var tokenHandler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-
-            return tokenHandler.WriteToken(token);
-        }
-
-        public class LoginRequest
-        {
-            public string Username { get; set; }
-            public string Password { get; set; }
-        }
     }
 }
--- DFCommonLib/Utils/DFCrypt.cs
+++ DFCommonLib/Utils/DFCrypt.cs
@@ -3,6 +3,15 @@ using System.Text;
 using System;
 using System.Collections.Generic;
 
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DFCommonLib.HttpApi;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using DFCommonLib.Utils;
+
 namespace DFCommonLib.Utils
 {
     public class DFCrypt
@@ -18,6 +27,25 @@ namespace DFCommonLib.Utils
             var data = Convert.FromBase64String(encodedString);
             string decodedString = Encoding.UTF8.GetString(data);
             return decodedString;
+        }
+
+        // JWT Token Generation
+        public static string GenerateJwtToken(string secret, IList<KeyValuePair<string, string>> claims)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(claims.Select(c => new System.Security.Claims.Claim(c.Key, c.Value))),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
