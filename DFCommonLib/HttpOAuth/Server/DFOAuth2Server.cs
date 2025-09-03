92ffc259863155dc3c1e6ff5792e7968ce0e2245
Thor Hanssen <thor.richard.hansen@gmail.com>
2025-09-03 17:51:05 +0200

    Setup swagger api key

--- DFCommonLib/HttpOAuth/Server/DFOAuth2Server.cs
+++ DFCommonLib/HttpOAuth/Server/DFOAuth2Server.cs
@@ -3,6 +3,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DFCommonLib.HttpApi.OAuth2
 {
@@ -32,7 +34,41 @@ namespace DFCommonLib.HttpApi.OAuth2
                         ValidateIssuerSigningKey = true
                     };
                 });
- 
+
+        }
+
+        public static void SetupSwaggerApi(IServiceCollection services)
+        {
+            // register the swagger generator
+            services.AddSwaggerGen( c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Account API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "Bearer",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new string[] {}
+                    }
+                });
+            });
         }
     }
 }
