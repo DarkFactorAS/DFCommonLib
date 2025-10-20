3741ce36f7b15fd152d9f2aadefe21767eb982a9
Thor Hanssen <69870370+Chaooz@users.noreply.github.com>
2025-10-20 07:55:16 +0200

    Develop (#23)
    * Minor fix to the logging for OAuth2
    
    * Updated MySQL from 9.3.0 to 9.4.0
    
    * Version 1.6.7

--- DFCommonLib.TestAppServer/Program.cs
+++ DFCommonLib.TestAppServer/Program.cs
@@ -23,7 +23,7 @@ namespace DFCommonLib.TestAppServer
     public class Program
     {
         public static string AppName = "DFCommonLib.TestAppServer";
-        public static string AppVersion = "1.6.6";
+        public static string AppVersion = "1.6.7";
 
         public static void Main(string[] args)
         {
